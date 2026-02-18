d583a63b9e9f124d9a06401f8a7bccf5f2532768
Your Name <you@example.com>
2026-02-18 22:51:27 +0200

    textareablog

--- resources/views/pages/admin/blog/create.blade.php
+++ resources/views/pages/admin/blog/create.blade.php
@@ -13,17 +13,17 @@
                 <label class="formLabel" for="editName">Blog Description</label>
                 <input class="formInputProfile" type="text" id="editName" name="description" >
                 <label class="formLabel" for="editEmail">Content</label>
-{{--                <!-- Place the first <script> tag in your HTML's <head> -->--}}
-{{--                <script src="https://cdn.tiny.cloud/1/pgal37qe4cgquwsgonvp0ix3ld3pjrui1uioy8u9v5p02mt9/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>--}}
+                <!-- Place the first <script> tag in your HTML's <head> -->
+                <script src="https://cdn.tiny.cloud/1/pgal37qe4cgquwsgonvp0ix3ld3pjrui1uioy8u9v5p02mt9/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 
-{{--                <!-- Place the following <script> and <textarea> tags your HTML's <body> -->--}}
-{{--                <script>--}}
-{{--                    tinymce.init({--}}
-{{--                        selector: 'textarea',--}}
-{{--                        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',--}}
-{{--                        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',--}}
-{{--                    });--}}
-{{--                </script>--}}
+                <!-- Place the following <script> and <textarea> tags your HTML's <body> -->
+                <script>
+                    tinymce.init({
+                        selector: 'textarea',
+                        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+                        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+                    });
+                </script>
                 <textarea name="blogContent">
 
                 </textarea>
