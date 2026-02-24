7f5bb8acb02f1c952c93aeed0a3ff36f5eda17cf
Your Name <you@example.com>
2026-02-24 23:31:23 +0200

    style bug fixes

--- resources/views/frontIncludes/responsiveNav.blade.php
+++ resources/views/frontIncludes/responsiveNav.blade.php
@@ -1,12 +1,12 @@
 <div id="responsiveNav">
     <div id="responsiveNavMenu">
-        <div class="responsiveNavItem"><a href="{{route("home")}}" class="menuColor">Home</a></div>
+        <div class="responsiveNavItem"><a href="/" class="menuColor">Home</a></div>
         <div class="responsiveNavItem"><a href="{{route("front.faq")}}" class="menuColor">Please Read Faq's</a></div>
 {{--        <div class="responsiveNavItem"><a href="{{route("front.products")}}" class="menuColor">Online Courses</a></div>--}}
         <div class="responsiveNavItem"><a href="{{route("front.team")}}" class="menuColor">Team Training</a></div>
         <div class="responsiveNavItem"><a href="{{route("front.consulting")}}" class="menuColor">Consulting</a></div>
         <div class="responsiveNavItem"><a href="{{route("front.blog")}}" class="menuColor">Blog</a></div>
         <div class="responsiveNavItem"><a href="{{route("front.contact")}}" class="menuColor">Contact Us</a></div>
-        
+
     </div>
 </div>
